c4fa7fd89be335f1583ef1d3c20ecf398f3b217a
zhanpei <zhangzhanpei@gmail.com>
2016-02-02 19:13:13 +0800

    单例模式
    Signed-off-by: zhanpei <zhangzhanpei@gmail.com>

--- 单例模式/single.php
+++ 单例模式/single.php
@@ -1,51 +1,33 @@
 <?php
 /*
-单例模式（用途：比如读取单个配置文件）
-
-self指向当前类
-parent指向父类
-this指向当前对象
+单例模式：保证一个类仅有一个实例，并对外提供一个全局访问点[场景：读取单个配置文件/数据库单一连接]
+多线程时仍然可能生成多个实例，要在getInstance()中判断实例部分加锁[php-fpm为阻塞单线程模型，无需考虑这个问题]
 */
 
-// 懒汉模式：调用getInstance时才返回实例
 class Singler
 {
-	private static $instance;
-	public static function getInstance()
-	{
-		if (self::$instance == null) {
-			self::$instance = new Singler;//无实例时初始化一个
-		}
-		return self::$instance;	//直接返回实例
-	}
-}
+    private static $instance;
 
-// 饿汉模式：声明static $instance时就创建实例，该实例属于类，加载类的时候就已经有实例了，不用等到调用getInstance()
-class Singler
-{
-	private static $singler = new Singler;
-	public static function getInstance()
-	{
-		return self::$singler;	//直接返回实例
-	}
-}
+    //禁止外部用new来实例化对象
+    private function __construct()
+    {
 
+    }
+
+    public static function getInstance()
+    {
+        if (self::$instance == null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+}
 
 $s1 = Singler::getInstance();
 $s2 = Singler::getInstance();
 
 if ($s1 === $s2) {
-	echo '单例';
+    echo '单例';
 } else {
-	echo '多实例';
-}
-
-echo "<br/>";
-
-$s3 = new Singler;//直接new的是多个实例（可以将构造函数设置成私有防止直接new）
-$s4 = new Singler;
-if ($s3 === $s4) {
-	echo '单例';
-} else {
-	echo '多实例';
+    echo '多实例';
 }
